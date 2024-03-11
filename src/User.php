c5470c64f83d479cb8b7ee504b617b944fb06955
Laura Ramos <ramoslaura418@gmail.com>
2024-03-11 11:33:38 -0600

    testing static methods with refactor

--- src/User.php
+++ src/User.php
@@ -22,13 +22,26 @@ class User
     protected $user_id;
 
     /**
+     * Email address
+     * @var string
+     */
+    public $email;
+
+    /**
+     * Mailer object
+     * @var Mailer
+     */
+    protected $mailer;
+
+    /**
      * Constructor
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(string $email)
     {
         $this->user_id = rand();
+        $this->email = $email;
     }
 
     public function getFullName()
@@ -50,4 +63,14 @@ class User
     {
        return $number;
     }
+
+    public function setMailer(Mailer $mailer) {
+        $this->mailer = $mailer;
+    }
+
+    public function notify(string $message)
+    {
+        return $this->mailer->send($this->email, $message);
+    }
+
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,38 +4,40 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+	private $user;
+	protected function setUp(): void
+    {
+        $this->user = new User('ramoslaura418@gmail.com');
+    }
+	
     public function testReturnsFullName()
     {
-        $user = new User;
 
-        $user->first_name = "Laura";
-        $user->surname = "Ramos";
+        $this->user->first_name = "Laura";
+        $this->user->surname = "Ramos";
 
-        $this->assertEquals('Laura Ramos', $user->getFullName());
+        $this->assertEquals('Laura Ramos', $this->user->getFullName());
     }
 
     public function testFullNameIsEmptyByDefault()
     {
-        $user = new User;
-
-        $this->assertEquals('', $user->getFullName());
+        $this->assertEquals('', $this->user->getFullName());
     }
 
-    public function testPrivateMethodInheritance()
+    /*public function testPrivateMethodInheritance()
 	{
 		$user = new UserChild();
 		$this->assertIsInt($user->getToken());
 
-	}
+	}*/
 
     public function testPrivateMethodGetID()
 	{
-		$user = new User();
 		$reflector = new ReflectionClass('User');
 		$method = $reflector->getMethod('getID');
 		$method->setAccessible(true);
 
-		$result = $method->invoke($user);
+		$result = $method->invoke($this->user);
 
 		$this->assertEquals(50, $result);
 	}
@@ -44,25 +46,36 @@ class UserTest extends TestCase
 	{
 		$number = 1;
 
-		$user = new User();
-		$reflector = new ReflectionClass($user);
+		$reflector = new ReflectionClass($this->user);
 		$method = $reflector->getMethod('getPrivateMethod');
 		$method->setAccessible(true);
 
-		$result = $method->invokeArgs($user, array($number));
+		$result = $method->invokeArgs($this->user, array($number));
 
 		$this->assertEquals(1, $result);
 	}
 
     public function testIdIsAnInteger()
 	{
-        $user = new User();
-		$reflection = new \ReflectionClass($user);
+		$reflection = new \ReflectionClass($this->user);
         $property = $reflection->getProperty('user_id');
         $property->setAccessible(true);
-        $value = $property->getValue($user);
+        $value = $property->getValue($this->user);
 
 		$this->assertIsInt($value);
 
 	}
+
+	public function testNotifyReturnsTrue()
+    {
+        $mailer = $this->createMock(Mailer::class);
+        
+        $mailer->expects($this->once())
+               ->method('send')
+               ->willReturn(true);
+        
+		$this->user->setMailer($mailer);
+        
+        $this->assertTrue($this->user->notify('Hola!'));
+    }
 }
