0fdc2b7160468dd8896392a7d363fe09bb8d112f
Laura Ramos <ramoslaura418@gmail.com>
2024-03-12 10:24:12 -0600

    add additionProvider

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class ExampleTest extends TestCase
 {
@@ -9,5 +10,21 @@ class ExampleTest extends TestCase
   {
     $this->assertEquals(4, 2 + 2);
   }
+
+  public static function additionProvider(): array
+    {
+      return [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 3],
+      ];
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testAdd(int $a, int $b, int $expected): void
+    {
+      $this->assertSame($expected, $a + $b);
+    }
 
 }
